26530f12d2a4c09da90cd9112b517ddb21dbd38b
Novruz Rahimov <novruzrehimov@gmail.com>
2024-04-23 12:11:58 +0400

    feat: custom upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,26 +1,18 @@
-"use client";
-
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "./../../utils/uploadthing";
-import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./upload-button";
 
 export function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>
         <div>Gallery</div>
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-4 ">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            endpoint="imageUploader"
-            onClientUploadComplete={() => router.refresh()}
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -1,4 +1,5 @@
 import {
+    generateReactHelpers,
     generateUploadButton,
     generateUploadDropzone,
   } from "@uploadthing/react";
@@ -6,4 +7,6 @@ import {
   import type { OurFileRouter } from "~/app/api/uploadthing/core";
    
   export const UploadButton = generateUploadButton<OurFileRouter>();
-  export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
+  
+export const {useUploadThing} = generateReactHelpers<OurFileRouter>();
