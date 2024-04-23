f6e0ca27d6fe3b65becbbadcdb014cee278bfca9
Novruz Rahimov <novruzrehimov@gmail.com>
2024-04-23 08:26:41 +0400

    feat: finalize modal & parellel image route

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - [x] "taint" (server-only)
 - [x] use Next/Image component
 - [x] Error management(/w Sentry)
-- [ ] Routing/image page(parellel route)
+- [x] Routing/image page(parellel route)
 - [ ] Delete button (/w Server Actions)
 - [ ] Analaytics (posthog)
 - [ ] Ratelimiting (upstash)
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,14 +1,27 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+
   return (
     <div className="flex h-full w-full min-w-0 items-center justify-center space-x-2">
       <div className="flex flex-shrink">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
-      <div className="flex w-96 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-96 flex-col border-l">
+        <div className="border-bottom p-2 text-center text-lg">
+          {image.name}
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created On:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
