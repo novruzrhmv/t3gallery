960453e6abfae37f70493d25624cde0da6bef7f0
Novruz Rahimov <novruzrehimov@gmail.com>
2024-04-23 08:17:08 +0400

    style: move to the grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,10 +30,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-          <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-          <TopNav />
-          {children}
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+        <body className={`font-sans ${inter.variable}`}>
+          <div className="grid h-screen grid-rows-[auto,1fr]">
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
+          </div>
           {modal}
           <div id="modal-root" />
         </body>
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,11 +3,11 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-    <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
+    <div className="flex h-full w-full min-w-0 items-center justify-center space-x-2">
+      <div className="flex flex-shrink">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col">
+      <div className="flex w-96 flex-col">
         <div className="text-xl font-bold">{image.name}</div>
       </div>
     </div>
